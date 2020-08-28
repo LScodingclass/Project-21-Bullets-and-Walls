60987c6f5d80a65d572aa498465c9a825d40238c
LScodingclass <69381279+LScodingclass@users.noreply.github.com>
2020-08-28 21:15:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet = createSprite(50, 200, 50, 20);
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80)
+  bullet.velocityX=speed;
+
+}
+
+function draw() {
+  background(255,255,255);
+  if (wall.x - bullet.x <= bullet.width/2 + wall.width/2){
+    bullet.velocityX=0;
+    var damage=(0.5*weight*speed*speed)/thickness*thickness*thickness;
+    if (damage>100){
+      wall.shapeColor=("red");
+    }
+    if (damage<100){
+      wall.shapeColor=("green");
+    }
+  } 
+  drawSprites();
+}
